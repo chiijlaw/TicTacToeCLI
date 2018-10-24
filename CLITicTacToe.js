d8010d4792390856e516ae5c9920c6e1ce22f04b
Chiijlaw <chiijlaw@berkeley.edu>
2018-10-24 14:19:27 -0700

    Players can type in a coordinate and it will change the game state.

--- CLITicTacToe.js
+++ CLITicTacToe.js
@@ -8,6 +8,7 @@ const rl = readline.createInterface({
 let board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
 let displayBoard = () => {
+  console.log("-----------------------");
   console.log(board[0]);
   console.log(board[1]);
   console.log(board[2]);
@@ -16,13 +17,29 @@ let displayBoard = () => {
 let winner = "none";
 let player = "X";
 
+let validateMove = (row, col) => {
+  if (board[row][col] === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 let ask = () => {
   rl.question(`Player ${player}, where would you like to go?`, answer => {
-    displayBoard();
-    if (player === "X") {
-      player = "O";
+    let row = answer[0];
+    let col = answer[2];
+    if (validateMove(row, col)) {
+      board[row][col] = player;
+      displayBoard();
+      if (player === "X") {
+        player = "O";
+      } else {
+        player = "X";
+      }
     } else {
-      player = "X";
+      console.log("Invalid move. Please try again");
+      ask();
     }
     if (winner === "none") {
       ask();
